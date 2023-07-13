57d2081f79a01ec4a97e131f8f87e0fad34b493c
Danny Thomas <danny.thomas@bell.net>
2023-07-12 21:06:38 -0400

    added google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,21 @@ export default function Document() {
           src="https://w.soundcloud.com/player/api.js"
           strategy="beforeInteractive"
         />
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-B6F063S4HV" />
+        <Script
+          id='google-analytics'
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-B6F063S4HV', {
+            page_path: window.location.pathname,
+          });
+        `,
+          }}
+        />
       </body>
     </Html>
   );
